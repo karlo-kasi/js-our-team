0a03e13585e73ea9df7c5c2a24fb85804779775b
karlo-kasi <karlokasi1995@gmail.com>
2025-01-23 17:39:58 +0100

    Add: Bonus

--- script.js
+++ script.js
@@ -42,13 +42,13 @@ const teamContainer = document.querySelector(".team-container")
 
 console.log(teamContainer)
 
+function addProfile() {
+  for (let i = 0; i < teamMembers.length; i++) {
+    let element = teamMembers[i]
 
-for (let i = 0; i < teamMembers.length; i++){
-     let element = teamMembers[i]
-
-     teamContainer.innerHTML += 
-     `<div class="card" style="width: 18rem;">
-                <img src="${element.img}" class="card-img-top" alt="${element.name}">
+    teamContainer.innerHTML +=
+      `<div class="card" style="width: 18rem;">
+                <img src="${element.img}" class="" alt="${element.name}">
                 <div class="card-body">
                     <h5 class="card-title">${element.name}</h5>
                     <p class="card-text">${element.role}</p>
@@ -57,11 +57,37 @@ for (let i = 0; i < teamMembers.length; i++){
             </div>
      `
     console.log(element)
+  }
 }
 
-let btn = document.getElementById ("button", function (event){
+addProfile()
 
-      event.
+const btn = document.getElementById('button') 
+//prendo gli input della compilazione del form
+btn.addEventListener ('click', function (event) {
+
+  event.preventDefault()
+
+  name = document.getElementById('nome').value
+  role = document.getElementById('role').value
+  email = document.getElementById('email').value
+  img = document.getElementById('img').value
+
+
+  teamMembers.push(
+    {
+      name,
+      role,
+      email,
+      img
+    }
+  )
+
+  console.log(teamMembers)
+  
+  teamContainer.innerHTML = ''
+
+  addProfile()
 
 
 })
